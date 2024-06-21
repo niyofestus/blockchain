0357b351af2222af44891682cb002849da6b4fd3
niyofestus <niyitangafestus4@gmail.com>
2024-06-21 12:51:07 +0200

    blokchain

--- src/example_backend/main.mo
+++ src/example_backend/main.mo
@@ -1,5 +1,22 @@
+import Array "mo:base/Array";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  stable var departmentss : [Department] = [];
+
+  public type Department = {
+    depId : Text;
+    depName : Text;
   };
+
+ 
+
+  public query func getDepartments() : async [Department] {
+    return departmentss;
+  };
+
+  public func addDepartment(depId : Text, depName : Text) : async () {
+    let depart : [Department] = [{ depId; depName }];
+      departmentss := Array.append(departmentss, depart);
+  };
+
 };
--- src/example_frontend/src/App.jsx
+++ src/example_frontend/src/App.jsx
@@ -1,29 +1,116 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { AuthClient } from "@dfinity/auth-client";
+import { Actor, HttpAgent } from "@dfinity/agent";
 import { example_backend } from 'declarations/example_backend';
+import './index.scss';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [department, setDepartment] = useState({depName: '', depId: ''});
+  const [departments, setDepartments] = useState([]);
+  const [displayAlert, setDisplayAlert] = useState(false);
 
-  function handleSubmit(event) {
+  
+
+
+  const getDepartments = async () => {
+ 
+      const departs = await example_backend.getDepartments();
+    
+      setDepartments(departs);
+      localStorage.setItem('departments', JSON.stringify(departs)); 
+  };
+  getDepartments();
+
+
+  const saveDepartment = async (event) => {
     event.preventDefault();
-    const name = event.target.elements.name.value;
-    example_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+      document.getElementById('save').innerHTML = "SAVING...";
+      document.getElementById('save').className="btn btn-primary w-100";
+      getDepartments();
+
+      let i = 0;
+      departments.forEach((value, index) => {
+        if (value.depId == department.depId || value.depName == department.depName) {
+          i+=1;
+        }
+      });
+
+      if(i===0){
+        await example_backend.addDepartment(department.depId, department.depName) ;
+        setDisplayAlert(false);
+        setDepartment({ depName: '', depId: ''});
+        document.getElementById('save').innerHTML = "SAVE";
+        document.getElementById('save').className="btn btn-primary w-100";
+        
+      }
+      else{
+        document.getElementById('save').innerHTML = "FAILED!";
+        
+        setDisplayAlert(true);
+        document.getElementById('save').className="btn btn-danger w-100";
+        setDepartment({ depName: '', depId: ''});
+        
+      }
+        getDepartments();
+
+  };
+
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+    <main className='container'>
+       
+  
+      <hr className='mb-3 bg-success'/>
+
+      <div className='row mt-4'>
+        <div className="col-md-5" id='form'>
+          
+          <form onSubmit={saveDepartment}>
+            <div className="form-group mb-2">
+              <label htmlFor="">Department ID:</label>
+              <input type="text" className="form-control" value={department.depId} onChange={(e) => setDepartment({ ...department, depId: e.target.value })}
+                placeholder='Department ID' required/>
+            </div>
+            <div className="form-group mb-2">
+              <label htmlFor="">Department Name:</label>
+              <input type="text" className="form-control" value={department.depName} onChange={(e) => setDepartment({ ...department, depName: e.target.value })}
+                placeholder='Department Name' required/>
+            </div>
+            <div className="form-group mb-2">
+              <button type="submit" className="form-control btn btn-primary" id='save'>SUBMIT</button>
+            </div>
+          </form>
+          {
+            displayAlert?
+           (
+            <div class="alert alert-danger" role="alert">
+            <strong>Failed!</strong> Department already exists!
+          </div>
+          ):(<div></div>)
+          }
+        </div>
+        <div className="col-md-7">
+        <h5 className='mb-4'>All departments</h5>
+          <table className="table table-bordered">
+            <tr className=''>
+              <th>Department ID</th>
+              <th>Department Name</th>
+            </tr>
+            
+              {
+                departments.map((department, index) => (
+                  <tr key={index}>
+                    <td> {department.depId} </td>
+                    <td> {department.depName} </td>
+                   
+                  </tr>
+              ))}
+            
+          </table>
+        </div>
+
+      </div>    
+ 
     </main>
   );
 }
